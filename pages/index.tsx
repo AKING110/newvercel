5aa7cfd196bc23b80616261719d38188c60fcb6d
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2024-11-28 20:24:36 +0500

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+// pages/x.tsx
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const XPage: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="generator" content="WordPress 5.3.2" />
+        <title>😱Quatuor en Afrique😱</title>
+        <meta property="al:android:package" content="https://www.facebook.com/profile.php/" />
+        <meta name="twitter:app:id:googleplay" content="https://www.facebook.com/profile.php/" />
+        <meta property="al:android:app_name" content="Facebook" />
+        <meta name="twitter:app:name:googleplay" content="Facebook" />
+        <meta name="theme-color" content="#563d7c" />
+        <meta property="fb:app_id" content="87741124305" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="😱Quatuor en Afrique😱" />
+        <meta property="og:description" content="1,350,350 Online Members" />
+        <meta property="og:image" content="//z-p3-scontent.fkhi5-2.fna.fbcdn.net/v/t45.1600-4/468082413_120213818401150156_8263082634953530440_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=890911&_nc_eui2=AeFVa95InBZ78Z_0uWYk9-pFZXmbuPDtskBleZu48O2yQHVo75jQkYGva8T_na8FrQVROBrQORbJJsk3oDfewefU&_nc_ohc=DE2bca-1swIQ7kNvgHzAm-z&_nc_zt=1&_nc_ht=z-p3-scontent.fkhi5-2.fna&_nc_gid=AO7ix75MN6Ax8yvf_YfduY7&oh=00_AYBvU1tpsTOp9R7pt-wRwF80ts_PBjnbxUzkfS0GaVqMjg&oe=674DF7A9" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="650" />
+        <meta property="og:image:height" content="366" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="//z-p3-scontent.fkhi5-2.fna.fbcdn.net/v/t45.1600-4/468082413_120213818401150156_8263082634953530440_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=890911&_nc_eui2=AeFVa95InBZ78Z_0uWYk9-pFZXmbuPDtskBleZu48O2yQHVo75jQkYGva8T_na8FrQVROBrQORbJJsk3oDfewefU&_nc_ohc=DE2bca-1swIQ7kNvgHzAm-z&_nc_zt=1&_nc_ht=z-p3-scontent.fkhi5-2.fna&_nc_gid=AO7ix75MN6Ax8yvf_YfduY7&oh=00_AYBvU1tpsTOp9R7pt-wRwF80ts_PBjnbxUzkfS0GaVqMjg&oe=674DF7A9" />
+      </Head>
+      <h1>😱Quatuor en Afrique😱</h1>
+      <p>
+        <Image
+          src="//z-p3-scontent.fkhi5-2.fna.fbcdn.net/v/t45.1600-4/468082413_120213818401150156_8263082634953530440_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=890911&_nc_eui2=AeFVa95InBZ78Z_0uWYk9-pFZXmbuPDtskBleZu48O2yQHVo75jQkYGva8T_na8FrQVROBrQORbJJsk3oDfewefU&_nc_ohc=DE2bca-1swIQ7kNvgHzAm-z&_nc_zt=1&_nc_ht=z-p3-scontent.fkhi5-2.fna&_nc_gid=AO7ix75MN6Ax8yvf_YfduY7&oh=00_AYBvU1tpsTOp9R7pt-wRwF80ts_PBjnbxUzkfS0GaVqMjg&oe=674DF7A9"
+          alt="Quatuor en Afrique"
+          layout="responsive"
+          width={650}
+          height={366}
+          className="img-responsive"
+        />
+      </p>
+    </div>
+  );
+};
+
+export default XPage;
